e97fcbaa81aeb7d76f5e6c3fb7d227749d255e61
N-Raykov <nikolaradoslavovraykov@gmail.com>
2023-05-11 15:20:51 +0200

    Final changes

--- 004_Engine_setup/MyGame.cs
+++ 004_Engine_setup/MyGame.cs
@@ -9,18 +9,39 @@ public class MyGame : Game
 
 	public Camera camera;
     public TeleportManager teleportManager;
-
+    Player player;
 	public MyGame() : base(1200, 800, false,false)
 	{
-        RenderMain = false;
+        //RenderMain = false;
 
+        //camera = new Camera(0, 0, 1200, 800);
+
+        //teleportManager = new TeleportManager();
+
+        //LoadLevel(startLevel);
+
+        //OnAfterStep += CheckLoadLevel;
+
+        RenderMain = false;
+        player = new Player(new Vec2(750, 750), 30);
         camera = new Camera(0, 0, 1200, 800);
+        player.AddChild(camera);
 
         teleportManager = new TeleportManager();
+        AddChild(player);
+        AddChild(new Line(new Vec2(0, 500), new Vec2(500, 1000)));
+        AddChild(new Line(new Vec2(500, 1000), new Vec2(1500, 1000)));
+        AddChild(new Line(new Vec2(1500, 1000), new Vec2(2000, 1300)));
+        AddChild(new Line(new Vec2(2000, 1600), new Vec2(2500, 1600)));
 
-        LoadLevel(startLevel);
 
-        OnAfterStep += CheckLoadLevel;
+        AddChild(new Line(new Vec2(500, 600), new Vec2(1500, 600)));
+
+
+
+        //AddChild(new BouncyFloor(new Vec2(600,900),new Vec2(1000,900)));
+        //AddChild(new CircleMapObject(30, new Vec2(1000, 500)));
+        AddChild(new CircleMapObject(30, new Vec2(250, 250)));
     }
 
     void DestroyAll()
