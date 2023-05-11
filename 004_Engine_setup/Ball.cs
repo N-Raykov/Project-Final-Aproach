c72da5befd2b52e11efd9a6762d0fbcdf17ae012
N-Raykov <nikolaradoslavovraykov@gmail.com>
2023-05-11 14:11:25 +0200

    Added balls

---
+++ 004_Engine_setup/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using TiledMapParser;
+
+using GXPEngine.Core;
+using GXPEngine;
+
+class Ball : AnimationSprite
+{
+    CircleMapObject ball;
+    bool created = false;
+    int radius = 30;
+
+    public Ball(TiledObject obj = null) : base("tilesheet.png", 1, 1, -1, false, false)
+    {
+        Initialize(obj);
+    }
+
+    void Initialize(TiledObject obj)
+    {
+        alpha = 0.0f;
+
+        Vec2 position = new Vec2(obj.X, obj.Y);
+
+        ball = new CircleMapObject(radius, position);
+    }
+
+    void Update()
+    {
+        if (created == false)
+        {
+            parent.AddChild(ball);
+            created = true;
+        }
+    }
+}
